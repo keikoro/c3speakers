36d31121b47ab5101ed11f03b654ec4f6c0d3cd1
K Kollmann <git@k.kollmann.moe>
2015-12-26 02:58:28 +0100

    write DB (create table), code cleanup

--- c3speakers.py
+++ c3speakers.py
@@ -1,8 +1,9 @@
-from urllib.request import urlopen
-from urllib.error import *
-from c3urls import *
-from datetime import date
 import sqlite3 as lite
+from datetime import date
+from urllib.error import *
+from urllib.request import urlopen
+
+from c3urls import *
 
 
 def hello_world():
@@ -12,24 +13,26 @@ def hello_world():
 
 
 def read_html():
-    """Open website that contains info on speakers."""
+    """Open site that contains info on C3 speakers."""
     try:
         html = urlopen('file:///' + parent)
-    except HTTPError as e:
+    except HTTPError as err:
         print("404 - The requested website is not available.")
-        print(e)
+        print(err)
         return
     get_speakers(html)
     return
 
 
 def congress_no(get_year=date.today().year):
-    """Return current year and congress shortcut."""
+    """Return current year and congress shortcut.
+    :param get_year: year YYYY
+    """
     c3 = 'C3'
     first = 1984
     this_year = int(get_year)
 
-    if (this_year >= 1984):
+    if this_year >= 1984:
         c3_no = this_year - first + 1
         c3_id = str(c3_no) + c3
         return this_year, c3_id
@@ -38,8 +41,10 @@ def congress_no(get_year=date.today().year):
 
 
 def db_connect(year=date.today().year):
-    """Create / connect to SQLite database."""
-    db_name = 'c3speakers' + str(year) + '.db'
+    """Create / connect to SQLite database.
+    :param year:
+    """
+    db_name = 'c3speakers' + str(year) + '.sqlite'
     db = lite.connect(db_name)
 
     # get SQLite version
@@ -49,6 +54,27 @@ def db_connect(year=date.today().year):
     print("SQLite version: %s" % data)
 
     return db_name
+
+
+def db_write(db_name):
+    """Update SQLite database.
+    :param db_name: name of the DB to operate on
+    """
+    db = lite.connect(db_name)
+
+    try:
+        cur = db.cursor()
+        # create table for speakers
+        cur.execute("""
+            CREATE TABLE speakers(id INTEGER PRIMARY KEY, name TEXT, twitter TEXT)
+        """)
+        db.commit()
+    except lite.OperationalError as err:
+        # rollback on problems with db statement
+        print(str(err))
+        db.rollback()
+    finally:
+        db.close()
 
 
 def get_speakers(html):
@@ -64,8 +90,14 @@ except ValueError as err:
     print(err.args[0])
 
 try:
-    db_name = db_connect(congress_data[0])
-    print(db_name)
+    # noinspection PyUnboundLocalVariable
+    db_file = db_connect(congress_data[0])
+    print(db_file)
 except NameError as err:
     print("Cannot create DB, no congress specified.")
-    raise err
+
+try:
+    # noinspection PyUnboundLocalVariable
+    print(db_write(db_file))
+except NameError as err:
+    print("Database with the provided name does not exist.")
