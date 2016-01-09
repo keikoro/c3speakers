7a97412a4441c53fe40ee8488152cc7168dd62d3
K Kollmann <git@k.kollmann.moe>
2016-01-09 01:21:55 +0100

    update setup.py with pypi classifiers

--- setup.py
+++ setup.py
@@ -7,6 +7,14 @@ setup(
     author='K Kollmann',
     author_email='code∆k.kollmann·moe',
     license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License'
+    ],
     keywords='Chaos Communicaton Congress, CCC, C3, speakers',
     install_requires=['beautifulsoup4', 'requests', 'twitter'],
     tests_require=['pytest', 'requests']
