3e522a279c01df5fac3ca2adc6210380d85153fa
K Kollmann <git@k.kollmann.moe>
2016-01-09 01:21:01 +0100

    add more info to setup.py file

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(
     name='c3speakers',
     version='1.0',
+    url='https://github.com/keikoro/c3speakers',
+    author='K Kollmann',
+    author_email='code∆k.kollmann·moe',
+    license='MIT',
+    keywords='Chaos Communicaton Congress, CCC, C3, speakers',
     install_requires=['beautifulsoup4', 'requests', 'twitter'],
     tests_require=['pytest', 'requests']
 )
